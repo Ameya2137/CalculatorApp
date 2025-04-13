6fe9d3aed996657e1a8b74c10e72be15557a7b34
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 15:22:22 +0530

    Update Division.java by creating a public class named Division

--- Operations/Division.java
+++ Operations/Division.java
@@ -5,3 +5,7 @@ package Operations;
 
 // Importing Scanner class to read user input
 import java.util.Scanner;
+
+// Creating a public class named Division
+public class Division {
+    public static void perform(Scanner sc) {
