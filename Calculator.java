738f906ad79a10fe743b00b8cd152fb2fc336f0b
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 14:51:32 +0530

    Update Calculator.java by defining a custom public class Calculator

--- Calculator.java
+++ Calculator.java
@@ -7,3 +7,11 @@
 // Importing Scanner class for input and custom Operations package
 import java.util.Scanner;
 import Operations.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Creating a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // A flag to keep the calculator running until the user chooses to exit
+        boolean keepRunning = true;
