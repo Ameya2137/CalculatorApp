aa407fd3b142afff5dc606f5c8931515f11c9fba
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 15:35:25 +0530

    Update SquareRoot.java by creating a public class named SquareRoot

--- Operations/SquareRoot.java
+++ Operations/SquareRoot.java
@@ -5,3 +5,7 @@ package Operations;
 
 // Importing Scanner class to read user input
 import java.util.Scanner;
+
+// Creating a public class named SquareRoot
+public class SquareRoot {
+    public static void perform(Scanner sc) {
