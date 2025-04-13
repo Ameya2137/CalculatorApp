9fed464a6fc3c52ecc5ff26f356c0186de8632fd
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 15:16:59 +0530

    Update Multiplication.java by creating a public class named Multiplication

--- Operations/Multiplication.java
+++ Operations/Multiplication.java
@@ -5,3 +5,7 @@ package Operations;
 
 // Importing Scanner class to read user input
 import java.util.Scanner;
+
+// Creating a public class named Multiplication
+public class Multiplication {
+    public static void perform(Scanner sc) {
