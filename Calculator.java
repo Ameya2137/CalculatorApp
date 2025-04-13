94b6fdfec1fc5a229f9c4a7047470b579549d8a1
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 14:48:18 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+// Calculator.java
+
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Dept: SY-AIML-A1
+
+// Importing Scanner class for input and custom Operations package
+import java.util.Scanner;
+import Operations.*;
