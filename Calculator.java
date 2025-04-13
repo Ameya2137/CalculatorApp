2da26162d03577d68950e080ef2b36080ad4a44f
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-13 14:52:16 +0530

    Update Calculator.java by defining menu operations

--- Calculator.java
+++ Calculator.java
@@ -15,3 +15,17 @@ public class Calculator {
         
         // A flag to keep the calculator running until the user chooses to exit
         boolean keepRunning = true;
+
+         // Loop until user chooses to exit (option 8)
+        while (keepRunning) {
+            // Displaying the menu
+            System.out.println("\n--- Calculator Menu ---");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Square");
+            System.out.println("6. Cube");
+            System.out.println("7. Square Root");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
